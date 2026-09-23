c2b0d88d5ff92a02b70506c56673e3d6ee89fd94
Manalisha Rajbanshi <binishrajbanshi0987@gmail.com>
2026-09-23 11:39:14 +0545

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>🎓 Smart Study Planner</h1>
+
+      <p>React + Firebase Project</p>
+
+      <h3>Day 1: Project Setup Complete</h3>
+    </div>
+  );
+}
+
+export default App;
